80a46d6450960ce2706b21c6133f3b753296009f
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-24 23:49:36 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -143,7 +143,7 @@ with open("qita.m3u", 'w', encoding='utf-8') as file:
 
 # 合并文件内容
 file_contents = []
-file_paths = ["cctv.txt", "weishi.txt", "IPTV.txt", "qita.txt"]  # 替换为实际的文件路径列表
+file_paths = ["cctv.txt", "weishi.txt", "ysyl.txt", "IPTV.txt", "qita.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
