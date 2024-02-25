6eef7adb357b8b154d56e511248eefe828ea3a52
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-26 02:23:46 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -122,7 +122,7 @@ print(f"{now_today}其他频道更新完成")
 
 # 合并文件内容
 file_contents = []
-file_paths = ["cctv.txt", "weishi.txt", "ktpd.txt", "ysyl.txt", "IPV6.txt", "qita.txt"]  # 替换为实际的文件路径列表
+file_paths = ["cctv.txt", "weishi.txt", "ktpd.txt", "ysyl.txt", "qita.txt", "IPV6.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
