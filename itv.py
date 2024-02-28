4541ffd65bc052fa0281315c98ab8ef7448703d8
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-28 17:24:00 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -81,7 +81,7 @@ for url in urls:
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_argument("blink-settings=imagesEnabled=false")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=chrome_options)
     # 使用WebDriver访问网页
     driver.get(url)  # 将网址替换为你要访问的网页地址
     time.sleep(10)
