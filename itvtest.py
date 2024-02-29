6141061df858575ac523ac768018f16562477cb5
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-29 11:16:59 +0800

    Update itvtest.py

--- itvtest.py
+++ itvtest.py
@@ -199,6 +199,5 @@ for file_path in file_paths:
 # 写入合并后的文件
 with open("itvlist.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
-output.close()
 # print(f"{now_today}写入合并后的文件")
 
