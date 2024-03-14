0891858d3a98020301253cacf3cf1a4f286d4c84
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-15 04:45:27 +0800

    Update hotel_ip_th.py

--- hotel_ip_th.py
+++ hotel_ip_th.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from queue import Queue
 from concurrent.futures import ThreadPoolExecutor
 
 import time
