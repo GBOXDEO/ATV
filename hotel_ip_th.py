9e570f6c8c9cbbfb4471f58387eb6656abea7fa9
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-15 07:04:14 +0800

    Update hotel_ip_th.py

--- hotel_ip_th.py
+++ hotel_ip_th.py
@@ -299,9 +299,12 @@ for ipv in sorted_list:
 # 等待所有任务完成
 task_queue.join()
 
-infoList = set(infoList)  # 去重得到唯一的URL列表
-infoList = sorted(infoList)
-results ＝ infoList
+# infoList = set(infoList)  # 去重得到唯一的URL列表
+# infoList = sorted(infoList)
+# 对频道进行排序
+infoList.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
+infoList.sort(key=lambda x: channel_key(x[0]))
+
 with open("myitv.txt", 'w', encoding='utf-8') as file:
     for info in infoList:
         file.write(info + "\n")
@@ -315,15 +318,11 @@ def channel_key(channel_name):
     else:
         return float('inf')  # 返回一个无穷大的数字作为关键字
 
-# 对频道进行排序
-results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
-results.sort(key=lambda x: channel_key(x[0]))
-
 result_counter = 8  # 每个频道需要的个数
 with open("cctv.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('【  央视频道  】,#genre#\n')
-    for result in results:
+    for result in infoList:
         channel_name, channel_url, speed = result
         if 'CCTV' in channel_name:
             if channel_name in channel_counters:
