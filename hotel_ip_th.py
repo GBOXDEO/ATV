d978764415deec11280049109390a8c101398a33
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-15 04:41:57 +0800

    Update hotel_ip_th.py

--- hotel_ip_th.py
+++ hotel_ip_th.py
@@ -206,7 +206,7 @@ def worker():
         task_queue.task_done()
  
 # 创建多个工作线程
-num_threads = 10
+num_threads = 15
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True) 
     t.start()
