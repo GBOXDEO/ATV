957f3ae18899e86f3b65a1a4e7e1bf160c6c09fc
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-17 02:45:28 +0800

    Update cctv.py

--- cctv.py
+++ cctv.py
@@ -81,6 +81,7 @@ with open("myitv.txt", 'r', encoding='utf-8') as file:
                 name = name.replace("香港音乐", "香港音乐卫视")
                 name = name.replace("高请", "")
                 name = name.replace("超", "")
+                name = name.replace("CCTVCCTV", "CCTV")
                 name = name.replace("汕头二台", "汕头经济生活")
                 name = name.replace("汕头二", "汕头经济生活")
                 name = name.replace("汕头一台", "汕头综合")
@@ -88,7 +89,8 @@ with open("myitv.txt", 'r', encoding='utf-8') as file:
                 name = name.replace("汕头三台", "汕头文旅体育")
                 name = name.replace("汕头台", "汕头综合")
                 name = name.replace("汕头生活", "汕头经济生活")
-                name = name.replace("CCTVCCTV", "CCTV")
+                name = name.replace("揭西台", "揭西")
+                name = name.replace("揭阳台", "揭阳综合")
                 urlright = channel_url[:4]
                 if urlright == 'http':
                     if '画中画' not in channel_name or '单音' not in channel_name or '直播' not in channel_name or '测试' not in channel_name or '主视' not in channel_name:
