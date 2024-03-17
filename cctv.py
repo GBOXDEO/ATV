49f6ad7470cc7df659a9e5db3d3c2e3c40090dd2
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-17 22:31:21 +0800

    Update cctv.py

--- cctv.py
+++ cctv.py
@@ -91,6 +91,7 @@ with open("myitv.txt", 'r', encoding='utf-8') as file:
                 name = name.replace("汕头三台", "汕头文旅体育")
                 name = name.replace("汕头台", "汕头综合")
                 name = name.replace("汕头生活", "汕头经济生活")
+                name = name.replace("汕头文化", "汕头文旅体育")
                 name = name.replace("揭西台", "揭西")
                 name = name.replace("揭阳台", "揭阳综合")
                 urlright = channel_url[:4]
