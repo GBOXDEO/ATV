f8467ef867796918454a159cd7268cb0d59bbe8e
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-03-24 20:08:12 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -14,9 +14,10 @@ def cut_first_chinese_words(text, num=2):
             return text[:i+2]
     return 'xxxxxxxxxxxxxxxxxx'
     
-all_text = "东莞 中山 佛山 顺德 南海 南方 宝安 岭南 广东 广州 广视 揭西 揭阳 汕头 汕尾 江门 深圳 清远 龙岗 湛江 潮州 珠江 粤语 肇庆 茂名 韶关 云浮 怀化 普宁 珠海 河源 香山 梅州 台球 足球 高尔夫 体育 网球 汽车 象棋 围棋 钓鱼 武术 汽摩 爱上 爱体 爱喜 爱奇 爱宠 爱幼 爱怀 爱悬 爱玩 爱生 爱电 爱科 爱经 爱谍 爱赛 爱都 爱院 爱青 牛哥 音乐"
-guangdong_text = "东莞 中山 佛山 顺德 南海 南方 宝安 岭南 广东 广州 广视 揭西 揭阳 汕头 汕尾 江门 深圳 清远 龙岗 湛江 潮州 珠江 粤语 肇庆 茂名 韶关 云浮 怀化 普宁 珠海 河源 香山 梅州"
+all_text = "东莞 中山 佛山 顺德 南海 南方 宝安 岭南 广东 广州 广视 揭西 揭阳 汕头 汕尾 江门 深圳 清远 龙岗 湛江 潮州 珠江 粤语 肇庆 茂名 韶关 云浮 怀化 普宁 珠海 河源 台球 足球 高尔夫 体育 网球 汽车 象棋 围棋 钓鱼 武术 汽摩 爱上 爱体 爱喜 爱奇 爱宠 爱幼 爱怀 爱悬 爱玩 爱生 爱电 爱科 爱经 爱谍 爱赛 爱都 爱院 爱青 牛哥 音乐 江苏 聚鲨 南京 盱眙 沛县 泰州 徐州 淮安 泗洪 东海 宿迁 常州 东海 响水 高淳 新沂 邳州 连云 睢宁 赣榆 水韵 贾汪"
+guangdong_text = "东莞 中山 佛山 顺德 南海 南方 宝安 岭南 广东 广州 广视 揭西 揭阳 汕头 汕尾 江门 深圳 清远 龙岗 湛江 潮州 珠江 粤语 肇庆 茂名 韶关 云浮 怀化 普宁 珠海 河源"
 tiyu_text = "台球 足球 高尔夫 体育 网球 汽车 象棋 围棋 钓鱼 武术 汽摩 爱上 爱体 爱喜 爱奇 爱宠 爱幼 爱怀 爱悬 爱玩 爱生 爱电 爱科 爱经 爱谍 爱赛 爱都 爱院 爱青 牛哥 音乐"
+js_txt="江苏 聚鲨 南京 盱眙 沛县 泰州 徐州 淮安 泗洪 东海 宿迁 常州 东海 响水 高淳 新沂 邳州 连云 睢宁 赣榆 水韵 贾汪"
 # 线程安全的队列，用于存储下载任务
 task_queue = Queue()
 lock = threading.Lock()
@@ -26,7 +27,7 @@ results = []
 
 channels = []
 error_channels = []
-headers={'User-Agent': 'okhttp/3.15 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+headers={'User-Agent': 'okhttp/3.12.10(Linux;Android9;V2049ABuild/TP1A.220624.014;wv)AppleWebKit/537.36(KHTML,likeGecko)Version/4.0Chrome/116.0.0.0MobileSafari/537.36'}
 se=requests.Session()
 
 with open("itv.txt", 'r', encoding='utf-8') as file:
@@ -58,7 +59,7 @@ def worker():
     while True:
         # 从队列中获取一个任务
         channel_name, channel_url = task_queue.get()
-        if ".m3u8" in channel_url or ".flv" in channel_url or ".mp4" in channel_url:
+        if "m3u8" in channel_url or "flv" in channel_url:
             try:
                 channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
                 lines = requests.get(channel_url,headers=headers, timeout=3, stream=True).text.strip().split('\n')  # 获取m3u8文件内容
@@ -69,7 +70,7 @@ def worker():
                 # 多获取的视频数据进行5秒钟限制
                 with eventlet.Timeout(5, False):
                     start_time = time.time()
-                    content = requests.get(ts_url,headers=headers, timeout=(2,5), stream=True).content
+                    content = requests.get(ts_url,headers=headers, timeout=(1,4), stream=True).content
                     end_time = time.time()
                     response_time = (end_time - start_time) * 1
     
@@ -79,10 +80,10 @@ def worker():
                     file_size = len(content)
                     # print(f"文件大小：{file_size} 字节")
                     download_speed = file_size / response_time / 1024
-                    print(f"下载速度：{download_speed:.3f} kB/s")
+                    # print(f"下载速度：{download_speed:.3f} kB/s")
                     normalized_speed = min(max(download_speed / 1024, 0.001), 100)  # 将速率从kB/s转换为MB/s并限制在1~100之间
                     #print(f'{channel_url}')
-                    #print(f"m3u8 标准化后的速率：{normalized_speed:.3f} MB/s")
+                    print(f"m3u8 标准化后的速率：{normalized_speed:.3f} MB/s {channel_url}")
     
                     # 删除下载的文件
                     os.remove(ts_lists_0)
@@ -96,7 +97,11 @@ def worker():
                     # print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
             except:
                 error_channel = channel_name, channel_url
-                # error_channels.append(error_channel)
+                # 获取锁
+                lock.acquire()
+                error_channels.append(error_channel)
+                # 释放锁
+                lock.release()
                 numberx = (len(results) + len(error_channels)) / len(channels) * 100
         else:
             try:
@@ -106,7 +111,7 @@ def worker():
                     for k in res.iter_content(chunk_size=2097152):
                         # 这里的chunk_size是1MB，每次读取1MB测试视频流
                         # 如果能获取视频流，则输出读取的时间以及链接
-                        if time.time()-now > 120:
+                        if time.time()-now > 20:
                             res.close()
                             print(f'Time out\t{channel_url}')
                             break
@@ -136,7 +141,7 @@ def worker():
         task_queue.task_done()
 
 # 创建多个工作线程
-num_threads = 30
+num_threads = 40
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True) 
     #t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
@@ -164,9 +169,25 @@ results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 now_today = datetime.date.today()
 # 将结果写入文件
 
-result_counter = 15  # 每个频道需要的个数
+result_counter = 16  # 每个频道需要的个数
 
 with open("qita.txt", 'w', encoding='utf-8') as file:
+    channel_counters = {}
+    file.write('【  江苏频道  】,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if '卫视' not in channel_name and 'CCTV' not in channel_name and '测试' not in channel_name and '电影' not in channel_name and '影院' not in channel_name and '剧场' not in channel_name and '影视' not in channel_name and '卡通' not in channel_name and '动漫' not in channel_name and '动画' not in channel_name and '少儿' not in channel_name:
+            if cut_first_chinese_words(channel_name) in js_txt:
+                if channel_name in channel_counters:
+                    if channel_counters[channel_name] >= result_counter:
+                        continue
+                    else:
+                        file.write(f"{channel_name},{channel_url}\n")
+                        channel_counters[channel_name] += 1
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] = 1
+                    
     channel_counters = {}
     file.write('【  广东频道  】,#genre#\n')
     for result in results:
