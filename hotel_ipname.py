c7da32f7e14f44679c7fad5562e05724000e04df
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-05-10 02:37:51 +0800

    Update hotel_ipname.py

--- hotel_ipname.py
+++ hotel_ipname.py
@@ -218,7 +218,7 @@ for i in diqu:
                 if "暂时失效" not in html_txt:
                     m3u8_div = result.find("a")
                     if m3u8_div:
-                        pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"  # 设置匹配的格式，如http://8.8.8.8:8888
+                        pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
                         urls_all = re.findall(pattern, m3u8_div.get('href'))
                         # print(urls_all)
                         if len(urls_all) > 0:
